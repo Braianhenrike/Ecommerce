485ee92c91b6fa68eaf0c9df265fe29737bf5e3e
Braian Henrique <80363931+Braianhenrike@users.noreply.github.com>
2024-01-08 17:50:59 -0300

    Criado botão que irá manipular o estado do componente de carrinho

--- Front/src/index.js
+++ Front/src/index.js
@@ -1,31 +1,30 @@
 import React from "react";
-import ReactDOM from "react-dom/client";
+import ReactDOM from "react-dom";
 import { BrowserRouter, Route, Switch, Redirect } from "react-router-dom";
-
-import UserLayout from "layouts/User/User.js";
-import AuthLayout from "layouts/Auth/Auth.js";
-
+import UserLayout from "./layouts/User/User";
+import AuthLayout from "./layouts/Auth/Auth";
 import "assets/scss/black-dashboard-react.scss";
 import "assets/demo/demo.css";
 import "assets/css/nucleo-icons.css";
 import "@fortawesome/fontawesome-free/css/all.min.css";
-
 import ThemeContextWrapper from "./components/ThemeWrapper/ThemeWrapper";
 import BackgroundColorWrapper from "./components/BackgroundColorWrapper/BackgroundColorWrapper";
+import { CartProvider } from "./components/UseCart/UseCart";  
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-
 
 root.render(
   <ThemeContextWrapper>
     <BackgroundColorWrapper>
-      <BrowserRouter>
-        <Switch>
-          <Route path="/user" render={(props) => <UserLayout {...props} />} />
-          <Route path="/auth" render={(props) => <AuthLayout {...props} />} />
-          <Redirect from="/" to="/auth/login" />
-        </Switch>
-      </BrowserRouter>
+      <CartProvider>
+        <BrowserRouter>
+          <Switch>
+            <Route path="/user" render={(props) => <UserLayout {...props} />} />
+            <Route path="/auth" render={(props) => <AuthLayout {...props} />} />
+            <Redirect from="/" to="/auth/login" />
+          </Switch>
+        </BrowserRouter>
+      </CartProvider>
     </BackgroundColorWrapper>
   </ThemeContextWrapper>
 );
